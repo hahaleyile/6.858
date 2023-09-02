78e8523fc2cda743c8aed313dfc9964ee5154435
hahaleyile <1025284546@qq.com>
2023-09-02 10:26:39 -0400

    Finish exercise 1

--- int-avg.py
+++ int-avg.py
@@ -2,32 +2,33 @@
 
 import z3
 
-## Construct two 32-bit integer values.  Do not change this code.
+# Construct two 32-bit integer values.  Do not change this code.
 a = z3.BitVec('a', 32)
 b = z3.BitVec('b', 32)
 
-## Compute the average of a and b.  The initial computation we provided
-## naively adds them and divides by two, but that is not correct.  Modify
-## these lines to implement your solution for both unsigned (u_avg) and
-## signed (s_avg) division.
+# Compute the average of a and b.  The initial computation we provided
+# naively adds them and divides by two, but that is not correct.  Modify
+# these lines to implement your solution for both unsigned (u_avg) and
+# signed (s_avg) division.
 ##
-## Watch out for the difference between signed and unsigned integer
-## operations.  For example, the Z3 expression (x/2) performs signed
-## division, meaning it treats the 32-bit value as a signed integer.
-## Similarly, (x>>16) shifts x by 16 bits to the right, treating it
-## as a signed integer.
+# Watch out for the difference between signed and unsigned integer
+# operations.  For example, the Z3 expression (x/2) performs signed
+# division, meaning it treats the 32-bit value as a signed integer.
+# Similarly, (x>>16) shifts x by 16 bits to the right, treating it
+# as a signed integer.
 ##
-## Use z3.UDiv(x, y) for unsigned division of x by y.
-## Use z3.LShR(x, y) for unsigned (logical) right shift of x by y bits.
-u_avg = z3.UDiv(a + b, 2)
-s_avg = (a + b) / 2
+# Use z3.UDiv(x, y) for unsigned division of x by y.
+# Use z3.LShR(x, y) for unsigned (logical) right shift of x by y bits.
+u_avg = (a & b)+z3.LShR((a ^ b), 1)
+s_avg = (a & b)+((a^b)>>1)
+s_avg = s_avg + (z3.LShR(s_avg,31) & (a^b))
 
-## Do not change the code below.
+# Do not change the code below.
 
-## To compute the reference answers, we extend both a and b by one
-## more bit (to 33 bits), add them, divide by two, and shrink back
-## down to 32 bits.  You are not allowed to "cheat" in this way in
-## your answer.
+# To compute the reference answers, we extend both a and b by one
+# more bit (to 33 bits), add them, divide by two, and shrink back
+# down to 32 bits.  You are not allowed to "cheat" in this way in
+# your answer.
 az33 = z3.ZeroExt(1, a)
 bz33 = z3.ZeroExt(1, b)
 real_u_avg = z3.Extract(31, 0, z3.UDiv(az33 + bz33, 2))
@@ -35,6 +36,7 @@ real_u_avg = z3.Extract(31, 0, z3.UDiv(az33 + bz33, 2))
 as33 = z3.SignExt(1, a)
 bs33 = z3.SignExt(1, b)
 real_s_avg = z3.Extract(31, 0, (as33 + bs33) / 2)
+
 
 def printable_val(v, signed):
     if type(v) == z3.BitVecNumRef:
@@ -44,11 +46,13 @@ def printable_val(v, signed):
             v = v.as_long()
     return v
 
+
 def printable_model(m, signed):
     vals = {}
     for k in m:
         vals[k] = printable_val(m[k], signed)
     return vals
+
 
 def do_check(msg, signed, avg, real_avg):
     e = (avg != real_avg)
@@ -64,6 +68,7 @@ def do_check(msg, signed, avg, real_avg):
         print("  Example:", printable_model(m, signed))
         print("  Your average:", printable_val(m.eval(avg), signed))
         print("  Real average:", printable_val(m.eval(real_avg), signed))
+
 
 do_check("unsigned avg", False, u_avg, real_u_avg)
 do_check("signed avg", True, s_avg, real_s_avg)
